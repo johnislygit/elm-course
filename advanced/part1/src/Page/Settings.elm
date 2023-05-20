28bb35d6977795e4045768bad7c64ac389fd8ea0
John Ly <johnlyeatsbanhmi@gmail.com>
2023-05-20 16:03:57 -0700

    Update Cred to opaque type with username

--- advanced/part1/src/Page/Settings.elm
+++ advanced/part1/src/Page/Settings.elm
@@ -15,7 +15,7 @@ import Json.Encode as Encode
 import Profile exposing (Profile)
 import Route
 import Session exposing (Session)
-import Username as Username exposing (Username)
+import Username exposing (Username)
 import Viewer exposing (Viewer)
 import Viewer.Cred as Cred exposing (Cred)
 
@@ -62,7 +62,7 @@ init session =
                     { avatar = Maybe.withDefault "" (Avatar.toMaybeString (Profile.avatar profile))
                     , email = Email.toString (Viewer.email viewer)
                     , bio = Maybe.withDefault "" (Profile.bio profile)
-                    , username = Username.toString cred.username
+                    , username = Username.toString (Cred.username cred)
                     , password = ""
                     }
 
